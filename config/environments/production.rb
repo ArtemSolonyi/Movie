1b8684ca9789b68e1246018265fffb1f667ad147
Artem Solonyi <igorartem123@gmail.com>
2022-03-17 19:11:46 +0300

    Initial Commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
 
-  config.assets.js_compressor = :uglifier
+
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
