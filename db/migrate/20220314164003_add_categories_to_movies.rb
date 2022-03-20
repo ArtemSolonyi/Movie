e4d73dc62d24c2b6889d2ef500e616ca944b56ba
Artem Solonyi <igorartem123@gmail.com>
2022-03-20 21:17:58 +0300

    Add Columns Categories and Ratings to Movies

--- db/migrate/20220314164003_add_categories_to_movies.rb
+++
@@ -1,4 +0,0 @@
-class AddCategoriesToMovies < ActiveRecord::Migration[7.0]
-  add_reference :ratings, :user, foreign_key: { to_table: :users }
-  add_reference :ratings, :movie, foreign_key: { to_table: :movies }
-end
