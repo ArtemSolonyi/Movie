30ab6271ed90cd1e752020a69634fd1f4f2312f8
Artem Solonyi <igorartem123@gmail.com>
2022-03-20 21:42:47 +0300

    initial commit

--- db/migrate/20220320181430_add_columns.rb
+++
@@ -1,6 +0,0 @@
-class AddColumns < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :movies, :category,foreign_key:{to_table: :categories}
-    add_reference :movies,:rating,foreign_key:{to_table: :ratings}
-  end
-end
