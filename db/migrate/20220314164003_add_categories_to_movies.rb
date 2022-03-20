282572f173ec7fa6f7caf62a2ecd58eb3e7a68bf
Artem Solonyi <igorartem123@gmail.com>
2022-03-20 21:01:43 +0300

    Remove Columns Categories and Rating

--- db/migrate/20220314164003_add_categories_to_movies.rb
+++ db/migrate/20220314164003_add_categories_to_movies.rb
@@ -1,4 +1,4 @@
 class AddCategoriesToMovies < ActiveRecord::Migration[7.0]
-  add_reference :ratings, :user_id, foreign_key: { to_table: :users }
-  add_reference :ratings, :movie_id, foreign_key: { to_table: :movies }
+  add_reference :ratings, :user, foreign_key: { to_table: :users }
+  add_reference :ratings, :movie, foreign_key: { to_table: :movies }
 end
